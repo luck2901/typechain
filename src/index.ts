9c8fda3acc6e1d63f9d3bafe96ea1b442be97ef8
한상현 <luck2901@naver.com>
2021-06-29 18:04:04 +0900

    Update/4

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,33 @@
-const sayHi = (name : string , age : number, gender:string) : string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+// interface Human {
+//     name: string;
+//     age: number;
+//     gender: string;
+// } //인터페이스는 TS에서만 사용 가능.
+
+// const person = {
+//     name: "sang",
+//     age: 25,
+//     gender: "male",
+// };
+
+class Human{
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name:string, age:number, gender:string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+} //react, node 등을 사용하면 class를 쓰게됨. 
+
+const lynn = new Human("Lynn", 18, "female");
+
+
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 
-console.log(sayHi("Sang", 25, "male"));
+console.log(sayHi(lynn));
 
 export { }; 
