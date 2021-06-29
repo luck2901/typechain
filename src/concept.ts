61a6177d39aa71b70dbe3471f9274a9a0f7d7652
한상현 <luck2901@naver.com>
2021-06-29 18:13:23 +0900

    Update/5

---
+++ src/concept.ts
@@ -0,0 +1,33 @@
+// interface Human {
+//     name: string;
+//     age: number;
+//     gender: string;
+// } //인터페이스는 TS에서만 사용 가능.
+
+// const person = {
+//     name: "sang",
+//     age: 25,
+//     gender: "male",
+// };
+
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+} //react, node 등을 사용하면 class를 쓰게됨. 
+
+const lynn = new Human("Lynn", 18, "female");
+
+
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(lynn));
+
+export { };
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,27 @@
-// interface Human {
-//     name: string;
-//     age: number;
-//     gender: string;
-// } //인터페이스는 TS에서만 사용 가능.
-
-// const person = {
-//     name: "sang",
-//     age: 25,
-//     gender: "male",
-// };
-
-class Human{
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name:string, age:number, gender:string) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(index: number,
+        previousHash: string,
+        hash: string,
+        data: string,
+        timestamp: number
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
-} //react, node 등을 사용하면 class를 쓰게됨. 
+}
 
-const lynn = new Human("Lynn", 18, "female");
+const genesisBlock: Block = new Block(0, "202020202020200", "", "Hello", 123456);
 
+let blockchain: [Block] = [genesisBlock];
 
-const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
-
-console.log(sayHi(lynn));
+console.log(blockchain);
 
 export { }; 
